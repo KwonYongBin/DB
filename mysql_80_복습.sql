375851ca7ed82eb2d191bd6f41b3b2e53ab1b526
KwonYongBin <dydqls6789@naver.com>
2025-07-22 18:26:30 +0900

    25.07.22

--- mysql_80_복습.sql
+++ mysql_80_복습.sql
@@ -49,25 +49,30 @@ SELECT * FROM EMPLOYEE;
 SELECT EMP_NAME, GENDER, HIRE_DATE FROM EMPLOYEE;
 
 -- 사원테이블의 사번, 사원명, 성별, 입사일, 급여를 조회
-
+select emp_id, emp_name, gender, hire_date, salary
+from employee;
 
 -- 부서테이블의 모든 정보 조회
-
+select *
+from employee;
 
 -- AS  : 컬럼명 별칭 부여 
 -- 형식> SELECT [컬럼명 as 별칭, ...] FROM [테이블명];
 -- 사원테이블의 사번, 사원명, 성별, 입사일, 급여 컬럼을 조회한 한글 컬럼명으로 출력
-
+select emp_id as '사번', emp_name as '사원명', gender as '성별', hire_date as '입사일', salary as '급여'
+from employee;
 
 -- 사원테이블의 ID, NAME, GENDER, HDATE, SALARY 컬럼명으로 조회
-
+select emp_id, emp_name, gender, hire_date, salary
+from employee;
 
 -- 사원테이블의 사번, 사원명, 부서명, 폰번호, 이메일, 급여, 보너스(급여*10%)를 조회
 -- 기존의 컬럼에 연산을 수행하여 새로운 컬럼을 생성할 수 있다!!
-
+select emp_id, emp_name, dept_id, phone, email, salary, salary*10, salary as bonus
+from employee;
 
 -- 현재 날짜를 조회 : CURDATE()
-
+select curdate() as date from dual;
 
 /**************************************
 	SELECT  : 테이블 내용 상세 조회
@@ -76,76 +81,113 @@ SELECT EMP_NAME, GENDER, HIRE_DATE FROM EMPLOYEE;
             WHERE [조건절];		 
 ***************************************/
 -- 정주고 사원의 정보를 조회
-
+select *
+from employee
+where emp_name = '정주고';
 
 -- SYS 부서에 속한 모든 사원을 조회 
-
+select *
+from employee
+where dept_id = 's';
 
 -- 사번이 S0005인 사원의 사원명, 성별, 입사일, 부서아이디, 이메일, 급여를 조회
-
+select emp_name, gender, hire_date, dept_id, email, salary
+from employee
+where emp_id = 's0005';
 
 -- SYS 부서에 속한 모든 사원들을 조회, 단 출력되는 EMP_ID 컬럼은 '사원번호' 별칭으로 조회
-
+select emp_id as '사원번호', emp_name, eng_name, gender, hire_date, retire_date, dept_id, phone, email, salary
+from employee
+where dept_id = 'sys';
 
 -- EMP_NAME '사원명' 별칭 수정
-
+select emp_id, emp_name as '사원명', eng_name, gender, hire_date, retire_date, dept_id, phone, email, salary
+from employee;
 
 -- !! WHERE 조건절 컬럼으로 별칭을 사용할 수 있을까요???
 -- 사원명이 홍길동인 사원을 별칭으로 조회??? ::: WHERE 조건절에서 별칭을 컬럼명으로 사용X 
-
+select *
+from employee
+where emp_name = '홍길동';
 
 -- 전략기획(STG) 부서의 모든 사원들의 사번, 사원명, 입사일, 급여를 조회
-
+select emp_id, emp_name, hire_date, salary, dept_id
+from employee
+where dept_id = 'stg';
 
 -- 입사일이 2014년 8월 1일인 사원들을 조회
-
+select *
+from employee
+where hire_date = '2014-08-01';
 
 -- 급여가 5000인 사원들을 조회
-
+select *
+from employee
+where salary = 5000;
 
 -- 성별이 남자인 사원들을 조회
+select *
+from employee
+where gender = 'm';
 
 -- 성별이 여자인 사원들을 조회
-
+select *
+from employee
+where gender = 'f';
 
 -- NULL : 아직 정의되지 않은 미지수
 -- 숫자에서는 가장 큰수로 인식, 논리적인 의미를 포함하고 있으므로 등호(=)로는 검색 X, IS 키워드와 함께 사용 O
 
 -- 급여가 NULL인 값을 가진 사원들을 조회
-
+select *
+from employee
+where salary is null;
 
 -- 영어이름이 정해지지 않은 사원들을 조회
-
+select *
+from employee
+where eng_name is null;
 
 -- 퇴사하지 않은 사원들을 조회
-
+select *
+from employee
+where retire_date is null;
 
 -- 퇴사하지 않은 사원들의 보너스 컬럼(급여*20%)을 추가하여 조회, 컬럼명은 BONUS
+select emp_id, emp_name, eng_name, gender, hire_date, retire_date, dept_id, phone, email, salary, salary*0.2 as bonus
+from employee
+where retire_date is null;
 
 -- 퇴사한 사원들의 사번, 사원명, 이메일, 폰번호, 급여를 조회
-
+select emp_id, emp_name, email, phone, salary,retire_date
+from employee
+where retire_date is not null;
 
 -- IFNULL 함수 : NULL 값을 다른 값은 대체하는 방법
 -- 형식> IFNULL(NULL포함컬럼명, 대체값)
 
 -- STG 부서에 속한 사원들의 정보 조회, 단, 급여가 NULL인 사원은 0으로 치환
-
+select emp_id, emp_name, gender, hire_date, retire_date, dept_id, phone, email, ifnull(salary, 0) as salary
+from employee
+where dept_id = 'stg';
 
 -- 사원 전체 테이블의 내용을 조회, 단 영어이름이 정해지지 않은 사원들은 'SMITH' 이름으로 치환
-SELECT EMP_ID, EMP_NAME, IFNULL(ENG_NAME, 'SMITH') AS ENG_NAME, HIRE_DATE, SALARY
-FROM EMPLOYEE;
+select emp_id, emp_name, ifnull(eng_name, 'smith') as eng_name, hire_date, salary
+from employee;
 
 -- MKT 부서의 사원들을 조회, 재직중인 사원들의 RETIRE_DATE 컬럼은 현재 날짜로 치환
-
+select emp_id, emp_name, eng_name, hire_date, salary, ifnull(retire_date, curdate()) as retire_date, dept_id
+from employee
+where dept_id = 'mkt';
 /**************************************
 	DISTINCT  : 중복된 데이터를 배제하고 조회
     형식> SELECT DISTINCT [컬럼리스트] ~				
 ***************************************/
 -- 사원테이블의 부서리스트를 조회
-
+select distinct dept_id from employee;
 
 -- 주의!! UNIQUE한 컬럼과 함께 조회하는 경우 DISTINCT가 적용되지 않음
-
+select distinct emp_id, dept_id from employee;
 
 /**************************************
 	ORDER BY 컬럼 : 데이터 정렬 
@@ -156,25 +198,41 @@ FROM EMPLOYEE;
 ***************************************/
 
 -- 급여를 기준으로 오름차순 정렬
-
+select *
+from employee
+order by salary asc;
 
 -- 모든 사원을 급여, 성별을 기준으로 오름차순 정렬
-
+select *
+from employee
+order by salary, gender asc;
 
 -- ENG_NAME이 널인 사원들을 입사일 기준으로 내림차순 정렬
-
+select *
+from employee
+where eng_name is null
+order by hire_date desc;
 
 -- 퇴직한 사원들을 급여기준으로 내림차순 정렬
-
+select *
+from employee
+where retire_date is not null
+order by salary desc;
 
 -- 퇴직한 사원들을 급여기준으로 내림차순 정렬, SALARY 컬럼을 '급여' 별칭으로 치환
 -- '급여' 별칭으로 ORDER BY가 가능할까요???  :: 별칭으로 ORDER BY 가능함
 --  WHERE 조건절 데이터 탐색 > 컬럼리스트 > 정렬
-
+select emp_id, emp_name, eng_name, gender, hire_date, retire_date, dept_id phonem, email, salary as '급여'
+from employee
+where retire_date is not null
+order by 급여 desc;
 
 -- 정보시스템(SYS) 부서 사원들 중 입사일이 빠른 순서, 급여를 많이 받는 순서로 정렬
 -- HIRE_DATE, SALARY 컬럼은 '입사일','급여' 별칭으로 컬럼리스트 생성 후 정렬
-
+select *
+from employee
+where dept_id = 'sys'
+order by hire_date asc, salary desc;
 
 /**************************************
 	조건절 + 비교연산자 : 특정 범위 혹은 데이터 검색
@@ -184,34 +242,53 @@ FROM EMPLOYEE;
 		 ORDER BY [컬럼명, ...] ASC/DESC
 ***************************************/
 -- 급여가 5000 이상인 사원들을 조회, 급여를 오름차순 정렬
-
+select *
+from employee
+where salary >= 5000
+order by salary asc;
 
 -- 2017-01-01 이후 입사한 사원들을 조회
-
+select *
+from employee
+where hire_date >= '2017-01-01';
 
 -- 입사일이 2015-01-01 이후이거나, 급여가 6000인 이상인 사원들을 조회  
 -- ~이거나, ~또는 : OR - 두 개의 조건중 하나만 만족해도 조회가능
-
+select *
+from employee
+where hire_date >= '2015-01-01' or salary >= 6000;
 
 -- 입사일이 2015-01-01 이후이고, 급여가 6000인 이상인 사원들을 조회  
 -- ~이고 : AND - 두 개의 조건이 모두 만족해야만 조회 가능
-
+select *
+from employee
+where hire_date >= '2015-01-01' and salary >= 6000;
 
 -- 특정 기간 : 2015-01-01 ~ 2017-12-31 사이에 입사한 모든 사원 조회
 -- 부서기준으로 오름차순 정렬
-
+select *
+from employee
+where hire_date >= '2015-01-01' and hire_date <= '2017-12-31'
+order by dept_id asc;
 
 -- 급여가 6000 이상 8000 이하인 사원들을 모두 조회
-
+select *
+from employee
+where salary >= 6000 and salary <= 8000;
 
 -- MKT 부서의 사원들의 사번, 사원명, 입사일, 이메일, 급여, 보너스(급여의 20%) 조회
 -- 급여가 NULL인 사원의 보너스는 기본 50
 -- 보너스가 1000 이상인 사원 조회
 -- 보너스가 높은 사원 기준으로 정렬
-
+select emp_id, emp_name, hire_date, email, salary, ifnull(salary*0.2 ,50) as bonus
+from employee
+where dept_id = 'mkt' and salary*0.2 >= 1000
+order by bonus desc;
 
 -- 사원명이 '일지매','오삼식','김삼순' 인 사원들을 조회
-
+select *
+from employee
+where emp_name = '일지매' or emp_name = '오삼식' or emp_name ='김삼순';
 
 /******************************************************
 	논리곱(AND) : BETWEEN ~ AND 
