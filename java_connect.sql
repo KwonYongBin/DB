e857cea2e0546285586b09fd7748d2151429bd9f
KwonYongBin <dydqls6789@naver.com>
2025-07-30 17:28:04 +0900

    25.07.30

---
+++ java_connect.sql
@@ -0,0 +1,23 @@
+use hrdb2019;
+select database();
+
+select
+	row_number() over() as rno,
+	emp_id, 
+	emp_name, 
+    dept_id, 
+    salary 
+from employee;
+
+--
+select * from employee;
+select emp_id, emp_name, salary, salary * 0.2 as bonus from employee;
+desc employee;
+
+select * from department;
+
+select  dept_id, 
+	dept_name, 
+	unit_id, 
+	start_date
+from department;
